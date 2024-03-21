3d9ec34091eee6f18a27f0a7a85943bcd7907caf
kristoboys <137424983+kristoboys@users.noreply.github.com>
2024-03-21 20:26:09 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-81b5f34f-ad10-40e3-92f7-415e481cef4a/)
 
 
 echo "$curl_output" >> Update.tsx
